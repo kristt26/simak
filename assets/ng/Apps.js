14badae52f3bae6d5fa244d975bfac584553b175
kristt26 <kristt26@gmail.com>
2020-02-24 14:45:06 +0900

    New Update

--- assets/ng/Apps.js
+++ assets/ng/Apps.js
@@ -333,32 +333,33 @@
             }
         })
         .controller("Login", function (
-            $scope, $http, $window, AuthService
+            $scope, $http, $window, AuthService, SweetAlert
         ) {
             $scope.DataInput = {};
             $scope.ProsesLogin = function (response) {
-                $http({
-                    method: "POST",
-                    url: AuthService.Base + "api/users/login",
-                    header: {
-                        "content-type": "application/json",
-                    },
-                    data: $scope.DataInput
-                }).then(function (response) {
-                    if (response.data.status)
-                        var Tampung = response.data;
-                    var j = Tampung.data;
-                    var Datarole = JSON.stringify(j.RoleUser.Role);
-                    $window.sessionStorage.setItem("Token", j.Token);
-                    $window.sessionStorage.setItem("Username", j.Username);
-                    $window.sessionStorage.setItem("Email", j.Email);
-                    $window.sessionStorage.setItem("NamaUser", j.NamaUser);
-                    $window.sessionStorage.setItem("Role", Datarole);
-                    window.location.href = "home.html";
-                }, function (error) {
-                    console.log(error);
-                    alert(error.data.message);
-                })
+                SweetAlert.swal("Approved!", "Sistem Lagi Maintenance", "success");
+                // $http({
+                //     method: "POST",
+                //     url: AuthService.Base + "api/users/login",
+                //     header: {
+                //         "content-type": "application/json",
+                //     },
+                //     data: $scope.DataInput
+                // }).then(function (response) {
+                //     if (response.data.status)
+                //         var Tampung = response.data;
+                //     var j = Tampung.data;
+                //     var Datarole = JSON.stringify(j.RoleUser.Role);
+                //     $window.sessionStorage.setItem("Token", j.Token);
+                //     $window.sessionStorage.setItem("Username", j.Username);
+                //     $window.sessionStorage.setItem("Email", j.Email);
+                //     $window.sessionStorage.setItem("NamaUser", j.NamaUser);
+                //     $window.sessionStorage.setItem("Role", Datarole);
+                //     window.location.href = "home.html";
+                // }, function (error) {
+                //     console.log(error);
+                //     alert(error.data.message);
+                // })
             }
         })
 
