80ea4a062c591aab189d77eb44a127d7179016ca
kristt26 <kristt26@gmail.com>
2020-05-27 19:24:11 +0900

    Tagihan

--- assets/ng/Directives/TagihanDirectives.js
+++ assets/ng/Directives/TagihanDirectives.js
@@ -8,16 +8,28 @@
 
             function getAction() {
                 var deferred = $q.defer();
-                $http({
-                    method: "GET",
-                    url: "http://stimik.ip-dynamic.com:8081/keuangan/api/datas/read/ReadDataPembayaran.php?npm=" + $window.sessionStorage.getItem("Username")
-                }).then(function (response) {
-                    service.message = response.data;
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "http://stimik.ip-dynamic.com:8081/keuangan/api/datas/read/ReadDataPembayaran.php?npm=" + $window.sessionStorage.getItem("Username"),
+                    "method": "GET",
+                    "headers": {}
+                  }
+                  
+                  $.ajax(settings).done(function (response) {
+                    service.message = response;
                     deferred.resolve(service.message);
-                }, function (error) {
-                    // console.log(error);
-                    deferred.reject(error);
-                })
+                  });
+                // $http({
+                //     method: "GET",
+                //     url: "http://stimik.ip-dynamic.com:8081/keuangan/api/datas/read/ReadDataPembayaran.php?npm=" + $window.sessionStorage.getItem("Username")
+                // }).then(function (response) {
+                //     service.message = response.data;
+                //     deferred.resolve(service.message);
+                // }, function (error) {
+                //     // console.log(error);
+                //     deferred.reject(error);
+                // })
                 return deferred.promise;
             }
 
