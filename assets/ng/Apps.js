600005c2957e9eec82def4adafec2d17f8420716
kristt26 <kristt26@gmail.com>
2020-07-18 05:06:29 +0900

    new

--- assets/ng/Apps.js
+++ assets/ng/Apps.js
@@ -151,6 +151,15 @@
         })
         .run(['uiSelect2Config', function (uiSelect2Config) {
             uiSelect2Config.placeholder = "Placeholder text";
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/5f09b9955b59f94722ba88ad/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
         }])
         
         .factory("AuthService", function ($window) {
